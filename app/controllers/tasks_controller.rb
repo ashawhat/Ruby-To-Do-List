18dcc3e9c49cd8cd040b393256c38d2da3940716
Emily Zhang & Ashley Rae Lee <student@epicodus.com>
2014-03-31 16:05:49 -0700

    able to check off tasks and see which ones are finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,53 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    @done_tasks = Task.where(:done => true)
+    @not_done_tasks = Task.where(:done => nil)
     render('tasks/index.html.erb')
   end
+
+  def new
+    @task = Task.new
+    render('tasks/new.html.erb')
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render('tasks/show.html.erb')
+  end
+
+  def create
+    @task = Task.create(name: params[:name])
+    if @task.save
+      render('tasks/success.html.erb')
+    else
+      render('tasks/new.html.erb')
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    render('tasks/edit.html.erb')
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(name: params[:name],
+                    done: params[:done])
+      render('tasks/success.html.erb')
+    else
+      render('tasks/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    render('tasks/destroy')
+  end
+
+  # def complete
+  #   @task = Task.find(params[:id])
+  #   @task.done
+  # end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ActiveRecord::Base
   validates :name, :presence => true
+
+  # def done
+  # @task = Task.find(params[:id])
+  # @task.done == true
+  # end
 end
